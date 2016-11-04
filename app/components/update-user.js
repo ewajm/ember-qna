34e8beb9a5f7c7b6833477bf5961b63ad27d62b3
Ewa Manek <manekej@gmail.com>
2016-11-04 11:48:34 -0700

    Move update user functions to a separate component

---
+++ app/components/update-user.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  isShowingModal: false,
+  actions: {
+    toggleModal(){
+      this.toggleProperty('isShowingModal');
+    },
+    updateUser(user){
+      var params = {
+        email: this.get('email'),
+        password: this.get('password')
+      };
+      this.set('email', "");
+      this.set('password', "");
+      this.set('isShowingModal', false);
+      this.sendAction('updateUser', user, params)
+    }
+  }
+});
--- app/components/user-display.js
+++ app/components/user-display.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   currentUser: Ember.inject.service(),
-  isShowingModal: false,
   isUser: Ember.computed('user.username', 'currentUser.curUser.username', function(){
     return this.get('user.username') === this.get('currentUser.curUser.username');
   }),
@@ -11,17 +10,7 @@ export default Ember.Component.extend({
       this.get('currentUser').logout();
       this.sendAction('logOut');
     },
-    toggleModal(){
-      this.set('isShowingModal', true);
-    },
-    updateUser(user){
-      var params = {
-        email: this.get('email'),
-        password: this.get('password')
-      };
-      this.set('email', "");
-      this.set('password', "");
-      this.set('isShowingModal', false);
+    updateUser(user, params){
       this.sendAction('updateUser', user, params)
     }
   }
