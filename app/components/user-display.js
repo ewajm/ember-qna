667db598314ccb7f24431385eb0ecf2e4359d84e
Ewa Manek <manekej@gmail.com>
2016-11-04 11:43:53 -0700

    Add ability to logout and edit profile from individual user page

--- app/components/user-display.js
+++ app/components/user-display.js
@@ -2,7 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   currentUser: Ember.inject.service(),
+  isShowingModal: false,
   isUser: Ember.computed('user.username', 'currentUser.curUser.username', function(){
     return this.get('user.username') === this.get('currentUser.curUser.username');
-  });
+  }),
+  actions: {
+    logOut(){
+      this.get('currentUser').logout();
+      this.sendAction('logOut');
+    },
+    toggleModal(){
+      this.set('isShowingModal', true);
+    },
+    updateUser(user){
+      var params = {
+        email: this.get('email'),
+        password: this.get('password')
+      };
+      this.set('email', "");
+      this.set('password', "");
+      this.set('isShowingModal', false);
+      this.sendAction('updateUser', user, params)
+    }
+  }
 });
--- app/helpers/user-icon.js
+++ app/helpers/user-icon.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function userIcon(params/*, hash*/) {
   var initial = params[0].slice(0, 1);
-  var iconUrl = 'https://dummyimage.com/100x100/333/fff.png&text=' + initial;
+  var iconUrl = 'https://dummyimage.com/100x100/745296/91f5ad.png&text=' + initial;
   return '<img class="img-responsive center-block" src="' + iconUrl + '">';
 }
 
